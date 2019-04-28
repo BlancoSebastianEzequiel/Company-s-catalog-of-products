bf9c69f2cb81c753904ed7fd5c96986d66b9eb89
sebastian <sebastian.e.blanco@gmail.com>
2019-04-27 22:08:25 -0300

    strat mongo in case is not running

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,6 +7,7 @@ flake8 server/
 echo "TESTING LINTER"
 pylint server/
 echo "UNIT TESTS"
+sudo systemctl start mongodb
 python -m pytest --cov=server
 echo "DELETING ALL __PYCACHE__ DIRECTORIES"
 find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
