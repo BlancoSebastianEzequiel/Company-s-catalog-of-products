eb88dd10e70a6ebd15ae465272e4c84590949f57
sebastian <sebastian.e.blanco@gmail.com>
2019-04-27 23:34:01 -0300

    test posting one user

--- server/tests/test_users.py
+++ server/tests/test_users.py
@@ -1,3 +1,6 @@
+import json
+
+
 def test_with_client(client):
     resp = client.get('/')
     assert resp.status_code == 200
@@ -8,4 +11,16 @@ def test_list_users_initially_empty(client):
     resp = client.get('/users/')
     assert not resp.json['data']
     assert isinstance(resp.json['data'], list)
-    assert resp.json['ok']
+
+
+def test_list_users_size_one_after_post_user(client):
+    client.post('/users/', data=json.dumps({
+        'first_name': 'Juan',
+        'last_name': 'perez',
+        'user_name': 'juanchi',
+        'mail': 'juanperez@gmail.com',
+        'password': '1234',
+        'dni': '39206786',
+    }), content_type='application/json')
+    resp = client.get('/users/')
+    assert len(resp.json['data']) == 1
