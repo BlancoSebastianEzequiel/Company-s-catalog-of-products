a6eece8dc892e26b98505a4db18d6362cd919c24
sebastian <sebastian.e.blanco@gmail.com>
2019-04-27 21:15:46 -0300

    delete generated pytest dirs and archives

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env sh
 echo "DELETING ALL __PYCACHE__ DIRECTORIES"
 find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+sudo rm -r .pytest_cache
 echo "TESTING FLAKE 8"
 flake8 server/
 echo "TESTING LINTER"
 pylint server/
 echo "UNIT TESTS"
 python -m pytest --cov=server
+echo "DELETING ALL __PYCACHE__ DIRECTORIES"
+find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+sudo rm -r .pytest_cache
