2eb2b883c0633b8805a7ea0f1abdcd804e3655db
sebastian <sebastian.e.blanco@gmail.com>
2019-05-04 14:43:26 -0300

    add tests

--- server/tests/test_users.py
+++ server/tests/test_users.py
@@ -25,8 +25,11 @@ def test_list_users_size_one_after_post_user(client):
         'type': 'client'
     }), content_type='application/json')
     assert resp.json['ok']
+    assert resp.status_code == HTTPStatus.OK
     resp = client.get('/users/')
     assert len(resp.json['data']) == 1
+    assert resp.json['ok']
+    assert resp.status_code == HTTPStatus.OK
 
 
 def test_post_user_with_no_first_name(client):
@@ -70,5 +73,43 @@ def test_deleting_posted_user(client):
     assert resp.status_code == HTTPStatus.OK
     _id = resp.json['data']
     resp = client.delete('/users/' + _id + '/')
+    print(f"resp: {resp.json}")
     assert resp.json['ok']
     assert resp.status_code == HTTPStatus.OK
+
+
+def test_deleting_not_existent_user(client):
+    bad_id = "not hex wrong len"
+    resp = client.delete('/users/' + bad_id + '/')
+    assert not resp.json['ok']
+    assert resp.status_code == HTTPStatus.BAD_REQUEST
+
+
+def test_updating_user(client):
+    post_resp = client.post('/users/', data=json.dumps({
+        'first_name': 'juan',
+        'last_name': 'perez',
+        'user_name': 'juanchi',
+        'mail': 'juanperez@gmail.com',
+        'password': '1234',
+        'dni': '39206786',
+        'type': 'client'
+    }), content_type='application/json')
+    assert post_resp.json['ok']
+    assert post_resp.status_code == HTTPStatus.OK
+    _id = post_resp.json['data']
+
+    patch_resp = client.patch('/users/', data=json.dumps({
+        '_id': _id,
+        'first_name': 'new name',
+    }), content_type='application/json')
+    print(patch_resp.json['data'])
+    assert patch_resp.json['ok']
+    assert patch_resp.status_code == HTTPStatus.OK
+    assert patch_resp.json['data'] == _id
+
+    get_resp = client.get('/users/')
+    print(get_resp.json['data'])
+    assert get_resp.json['ok']
+    assert get_resp.status_code == HTTPStatus.OK
+    assert get_resp.json['data'][0]['first_name'] == 'new name'
