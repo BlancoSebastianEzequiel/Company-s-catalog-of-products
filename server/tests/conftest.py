014f2f5397121735169f830b4448eb50ca075e52
sebastian <sebastian.e.blanco@gmail.com>
2019-05-13 10:44:59 -0300

    change return value to tuple with user data and secret password

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -16,21 +16,20 @@ def client():
     # Clear databases
     MONGO.db['users'].delete_many({})
     yield client
-    print("FIRST")
 
 
 @pytest.fixture
-def user_data():
-    print("SECOND")
+def random_user():
+    secret = fake.password()
     data = {
         'first_name': fake.pystr(),
         'last_name': fake.pystr(),
         'user_name': fake.pystr(),
-        'mail': fake.email(),
-        'password': fake.password(),
+        'email': fake.email(),
+        'password': secret,
         'dni': str(fake.random_number(8, True)),
         'type': fake.random_element(('client', 'admin'))
     }
     _id = UsersController.post(data)[0]['data']
     user_data = UsersController.get(_id)[0]['data']
-    yield user_data
+    yield user_data, secret
