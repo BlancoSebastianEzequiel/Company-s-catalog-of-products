e585520a19dde09d0563d4f60a961af17419d0e0
sebastian <sebastian.e.blanco@gmail.com>
2019-05-13 08:40:13 -0300

    add test to login a registered user

--- server/tests/test_session.py
+++ server/tests/test_session.py
@@ -11,3 +11,10 @@ def test_unregistered_user(client):
     assert not resp.json['ok']
     assert resp.status_code == http.BAD_REQUEST
     assert resp.json['data'] == "user does not exist"
+
+
+def test_login_of_registered_user(client, user_data):
+    resp = client.get('/users/')
+    assert len(resp.json['data']) == 1
+    assert resp.json['ok']
+    assert resp.status_code == http.OK
