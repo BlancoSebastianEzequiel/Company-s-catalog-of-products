ff8ba25057b8d8ad612e4a782745d566c74182ac
sebastian <sebastian.e.blanco@gmail.com>
2019-05-13 17:06:53 -0300

    add intallation of client dependencies

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+echo "--------------------------INSTALLING SERVER DEPENDENCIES--------------------------"
 pip install -r requirements.txt
+echo "--------------------------INSTALLING CLIENT DEPENDENCIES--------------------------"
+cd client && npm i && cd ../
