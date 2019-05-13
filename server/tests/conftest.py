048f707986d5a393deaa9f40d820f35021bb74fb
sebastian <sebastian.e.blanco@gmail.com>
2019-05-13 08:39:36 -0300

    add fixture to create a random user

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from faker import Faker
+from server.controller.users import UsersController
 
 from server.app import create_app
 from server.libs.mongo import MONGO
@@ -15,3 +16,21 @@ def client():
     # Clear databases
     MONGO.db['users'].delete_many({})
     yield client
+    print("FIRST")
+
+
+@pytest.fixture
+def user_data():
+    print("SECOND")
+    data = {
+        'first_name': fake.pystr(),
+        'last_name': fake.pystr(),
+        'user_name': fake.pystr(),
+        'mail': fake.email(),
+        'password': fake.password(),
+        'dni': str(fake.random_number(8, True)),
+        'type': fake.random_element(('client', 'admin'))
+    }
+    _id = UsersController.post(data)[0]['data']
+    user_data = UsersController.get(_id)[0]['data']
+    yield user_data
