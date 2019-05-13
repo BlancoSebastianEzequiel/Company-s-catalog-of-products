2245298fd9f392697f6f51976a028ed141d568be
sebastian <sebastian.e.blanco@gmail.com>
2019-05-13 17:11:11 -0300

    delete unused firebase key

--- conf/local.py
+++ conf/local.py
@@ -6,4 +6,3 @@ class Config(flask.Config):
     DEBUG = os.environ.get('ENV') == 'development'
     MONGO_URI = os.environ.get('DB', "mongodb://localhost:27017/development")
     SKIP_AUTH = True
-    FIREBASE_API_KEY = os.environ.get('FIREBASE_API_KEY')
