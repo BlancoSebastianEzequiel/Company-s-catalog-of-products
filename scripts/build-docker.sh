f1a0f965e633ef53d8458daf0f9fdb80d9e79654
sebastian <sebastian.e.blanco@gmail.com>
2019-05-14 18:38:04 -0300

    remove running containers and change way to build

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
+docker container stop $(docker container ls -aq)
+docker container rm $(docker container ls -aq)
 echo "BUILDING DOCKER IMAGE"
-docker build -t server-img .
+#docker build -t server-img .
+docker-compose up --build
 echo "LAUNCHING DOCKER CONTAINER"
 sh scripts/run_docker.sh
