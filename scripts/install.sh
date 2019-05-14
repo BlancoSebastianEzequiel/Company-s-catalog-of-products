da87ca5bafe4f0b6969ee7176f581da5ef1bb445
sebastian <sebastian.e.blanco@gmail.com>
2019-05-14 18:38:21 -0300

    remove npm

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "--------------------------INSTALLING SERVER DEPENDENCIES--------------------------"
 pip install -r requirements.txt
-echo "--------------------------INSTALLING CLIENT DEPENDENCIES--------------------------"
-cd client && npm i && cd ../
+#echo "--------------------------INSTALLING CLIENT DEPENDENCIES--------------------------"
+#cd client && npm i && cd ../
