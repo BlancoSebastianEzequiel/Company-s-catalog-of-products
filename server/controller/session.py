5ba2b7b243ff566ca8538cbb5fcbc3e88b782393
sebastian <sebastian.e.blanco@gmail.com>
2019-05-15 03:31:19 -0300

    check posible exception when generating token

--- server/controller/session.py
+++ server/controller/session.py
@@ -14,12 +14,17 @@ class Session:
             msg = "user does not exist"
             return {'data': msg, 'ok': False}, http.BAD_REQUEST
         user_data = res["data"][0]
+        user_data['_id'] = str(user_data['_id'])
         if check_password(secret_pass, user_data['password']):
-            token = jwt.encode(
-                user_data,
-                secret_pass,
-                algorithm='HS256'
-            ).decode()
-            return {'data': token, 'ok': True}, http.OK
+            try:
+                token = jwt.encode(
+                    user_data,
+                    secret_pass,
+                    algorithm='HS256'
+                ).decode()
+                return {'data': str(token), 'ok': True}, http.OK
+            except Exception as e:
+                print(f"exception: {e}")
+                return {'data': e, 'ok': False}, http.INTERNAL_SERVER_ERROR
         else:
             return {'data': "Unauthorized", 'ok': False}, http.UNAUTHORIZED
