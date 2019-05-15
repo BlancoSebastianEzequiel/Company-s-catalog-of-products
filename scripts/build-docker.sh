5f114f8276de82ce60deeb254a53fe03e65cae89
sebastian <sebastian.e.blanco@gmail.com>
2019-05-15 11:29:44 -0300

    build client and server image

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -2,7 +2,7 @@
 docker container stop $(docker container ls -aq)
 docker container rm $(docker container ls -aq)
 echo "BUILDING DOCKER IMAGE"
-#docker build -t server-img .
-docker-compose up --build
+docker build -t client-img . -f client/Dockerfile
+docker build -t server-img .
 echo "LAUNCHING DOCKER CONTAINER"
 sh scripts/run_docker.sh
