03a21ad32bccc5078e8329d164bf458332b1560d
sebastian <sebastian.e.blanco@gmail.com>
2019-05-15 03:27:37 -0300

    add which field should be unique and change constructor signature

--- server/model/users.py
+++ server/model/users.py
@@ -8,15 +8,25 @@ class Users(Model):
     schema = {
         'first_name': str,
         'last_name': str,
-        'user_name': str,
-        'email': str,
+        'user_name': str,  # unique value
+        'email': str,  # unique value
         'password': str,
-        'dni': str,
+        'dni': str,  # unique value
         'type': str
     }
 
-    def __init__(self, data: dict, _id=None, hash_pass=False):
+    unique_values = {
+        'first_name': False,
+        'last_name': False,
+        'user_name': True,
+        'email': True,
+        'password': False,
+        'dni': True,
+        'type': False
+    }
+
+    def __init__(self, data, _id=None, hash_pass=False, unique_values=False):
         if hash_pass:
             self.validate_key_in_schema('password')
             data['password'] = get_hashed_password(data['password'])
-        super().__init__(data, _id)
+        super().__init__(data, _id, check_unique_values=unique_values)
