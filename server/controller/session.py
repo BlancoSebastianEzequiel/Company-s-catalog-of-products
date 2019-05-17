ff80bc50a3ce98658d5f9dbb4c5265f93e7494c3
sebastian <sebastian.e.blanco@gmail.com>
2019-05-17 02:32:57 -0300

    change success status and token generator

--- server/controller/session.py
+++ server/controller/session.py
@@ -1,6 +1,6 @@
-import jwt
 from server.controller.users import UsersController
 from server.controller.utils import check_password
+from server.secrets import TOKEN_SERIALIZER
 from http import HTTPStatus as http
 
 
@@ -14,17 +14,12 @@ class Session:
             msg = "user does not exist"
             return {'data': msg, 'ok': False}, http.BAD_REQUEST
         user_data = res["data"][0]
-        user_data['_id'] = str(user_data['_id'])
+        _id = str(user_data['_id'])
         if check_password(secret_pass, user_data['password']):
             try:
-                token = jwt.encode(
-                    user_data,
-                    secret_pass,
-                    algorithm='HS256'
-                ).decode()
-                return {'data': str(token), 'ok': True}, http.OK
+                token = TOKEN_SERIALIZER.dumps({'_id': _id}).decode('utf-8')
+                return {'data': token, 'ok': True}, http.CREATED
             except Exception as e:
-                print(f"exception: {e}")
                 return {'data': e, 'ok': False}, http.INTERNAL_SERVER_ERROR
         else:
             return {'data': "Unauthorized", 'ok': False}, http.UNAUTHORIZED
