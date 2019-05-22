701d61a83d7f0260e1ae3d9a711aaf81d89f2644
sebastian <sebastian.e.blanco@gmail.com>
2019-05-22 06:18:56 -0300

    change serializer to one that does not have a time out

--- server/secrets.py
+++ server/secrets.py
@@ -1,6 +1,6 @@
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import URLSafeSerializer as Serializer
 
 SECRET_KEY = 's3cr3t'
-TOKEN_SERIALIZER = Serializer(SECRET_KEY, expires_in=3600)
+TOKEN_SERIALIZER = Serializer(SECRET_KEY)
 COMPANY_EMAIL = "ACPI.company.catalog.of.products@gmail.com"
 EMAIL_PASSWORD = "ACPI/SRL/1234"
