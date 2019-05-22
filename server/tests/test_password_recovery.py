0787e2a2f4d35421d080a6ff86f7df691673ec36
sebastian <sebastian.e.blanco@gmail.com>
2019-05-22 03:14:58 -0300

    add test to send email to recover the password and check the controller functioning

---
+++ server/tests/test_password_recovery.py
@@ -0,0 +1,55 @@
+import json
+from http import HTTPStatus
+
+
+def test_password_recovery_of_posted_user(client):
+    secret = '1234'
+    email = 'sebastian.e.blanco@gmail.com'
+    resp = client.post('/users/', data=json.dumps({
+        'first_name': 'juan',
+        'last_name': 'perez',
+        'user_name': 'juanchi',
+        'email': email,
+        'password': secret,
+        'type': 'client'
+    }), content_type='application/json')
+    assert resp.json['ok']
+    assert resp.status_code == HTTPStatus.CREATED
+    resp = client.get('/password_recovery/' + email + '/')
+    assert resp.json['ok']
+    assert resp.status_code == HTTPStatus.OK
+
+
+def test_password_recovery_with_not_bad_email(client):
+    resp = client.post('/users/', data=json.dumps({
+        'first_name': 'juan',
+        'last_name': 'perez',
+        'user_name': 'juanchi',
+        'email': 'sebastian.e.blanco@gmailcom',
+        'password': '1234',
+        'type': 'client'
+    }), content_type='application/json')
+    assert resp.json['ok']
+    assert resp.status_code == HTTPStatus.CREATED
+    resp = client.get('/password_recovery/' + 'bad_email' + '/')
+    assert not resp.json['ok']
+    assert resp.status_code == HTTPStatus.BAD_REQUEST
+
+
+def test_password_recovery_with_without_skiping_sending_email(auth_client):
+    secret = '1234'
+    email = 'sebastian.e.blanco@gmail.com'
+    resp = auth_client.post('/users/', data=json.dumps({
+        'first_name': 'juan',
+        'last_name': 'perez',
+        'user_name': 'juanchi',
+        'email': email,
+        'password': secret,
+        'type': 'client'
+    }), content_type='application/json')
+    assert resp.json['ok']
+    assert resp.status_code == HTTPStatus.CREATED
+    resp = auth_client.get('/password_recovery/' + email + '/')
+    assert resp.json['ok']
+    assert resp.status_code == HTTPStatus.OK
+
