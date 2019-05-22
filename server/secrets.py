1beb25179eb6396d553c507b3ed803b583b58020
sebastian <sebastian.e.blanco@gmail.com>
2019-05-22 03:16:16 -0300

    save the email and password company account

--- server/secrets.py
+++ server/secrets.py
@@ -2,3 +2,5 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 SECRET_KEY = 's3cr3t'
 TOKEN_SERIALIZER = Serializer(SECRET_KEY, expires_in=3600)
+COMPANY_EMAIL = "ACPI.company.catalog.of.products@gmail.com"
+EMAIL_PASSWORD = "ACPI/SRL/1234"
