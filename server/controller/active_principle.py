1bdd712051d026826e97520d8eb8fbeea35bed69
sebastian <sebastian.e.blanco@gmail.com>
2019-05-22 12:28:37 -0300

    inherit from abm generic controller

--- server/controller/active_principle.py
+++ server/controller/active_principle.py
@@ -1,35 +1,13 @@
 from http import HTTPStatus as http
 from server.model.active_principle import ActivePrinciple
 from server.exceptions.status_exception import StatusException
+from server.controller.abm_db_controller import ABMController
 
 
-class ActivePrincipleController:
+class ActivePrincipleController(ABMController):
 
-    @classmethod
-    def delete(cls, _id):
-        try:
-            data = ActivePrinciple.get(_id)
-            if data is None:
-                msg = "user does not exist"
-                return {'data': msg, 'ok': False}, http.BAD_REQUEST
-            _id = ActivePrinciple(data, _id=_id).delete()
-            return {'data': str(_id), 'ok': True}, http.OK
-        except StatusException as e:
-            return {'data': f"Error deleting: {e}", 'ok': False}, e.status
-
-    @classmethod
-    def get(cls, _id):
-        try:
-            return {'data': ActivePrinciple.get(_id), 'ok': True}, http.OK
-        except StatusException as e:
-            return {'data': f"Error getting one: {e}", 'ok': False}, e.status
-
-    @classmethod
-    def get_all(cls, args):
-        try:
-            return {'data': ActivePrinciple.get_all(args), 'ok': True}, http.OK
-        except StatusException as e:
-            return {'data': f"Error getting all: {e}", 'ok': False}, e.status
+    db = ActivePrinciple
+    name = 'active principle'
 
     @classmethod
     def post(cls, data):
@@ -38,17 +16,3 @@ class ActivePrincipleController:
             return {'data': str(_id), 'ok': True}, http.CREATED
         except StatusException as e:
             return {'data': f"Error posting: {e}", 'ok': False}, e.status
-
-    @classmethod
-    def patch(cls, data):
-        try:
-            _id = data.get('_id')
-            data.pop('_id')
-            active_principle_data = ActivePrinciple.get(_id)
-            if active_principle_data is None:
-                msg = "user does not exist"
-                return {'data': msg, 'ok': False}, http.BAD_REQUEST
-            _id = ActivePrinciple(active_principle_data, _id=_id).patch(data)
-            return {'data': str(_id), 'ok': True}, http.CREATED
-        except StatusException as e:
-            return {'data': f"Error patching: {e}", 'ok': False}, e.status
--- server/controller/products.py
+++ server/controller/products.py
@@ -1,35 +1,13 @@
 from http import HTTPStatus as http
 from server.model.products import Products
 from server.exceptions.status_exception import StatusException
+from server.controller.abm_db_controller import ABMController
 
 
-class ProductsController:
+class ProductsController(ABMController):
 
-    @classmethod
-    def delete(cls, _id):
-        try:
-            products_data = Products.get(_id)
-            if products_data is None:
-                msg = "user does not exist"
-                return {'data': msg, 'ok': False}, http.BAD_REQUEST
-            _id = Products(products_data, _id=_id).delete()
-            return {'data': str(_id), 'ok': True}, http.OK
-        except StatusException as e:
-            return {'data': f"Error deleting: {e}", 'ok': False}, e.status
-
-    @classmethod
-    def get(cls, _id):
-        try:
-            return {'data': Products.get(_id), 'ok': True}, http.OK
-        except StatusException as e:
-            return {'data': f"Error getting one: {e}", 'ok': False}, e.status
-
-    @classmethod
-    def get_all(cls, args):
-        try:
-            return {'data': Products.get_all(args), 'ok': True}, http.OK
-        except StatusException as e:
-            return {'data': f"Error getting all: {e}", 'ok': False}, e.status
+    db = Products
+    name = 'product'
 
     @classmethod
     def post(cls, data):
@@ -38,17 +16,3 @@ class ProductsController:
             return {'data': str(_id), 'ok': True}, http.CREATED
         except StatusException as e:
             return {'data': f"Error posting: {e}", 'ok': False}, e.status
-
-    @classmethod
-    def patch(cls, data):
-        try:
-            _id = data.get('_id')
-            data.pop('_id')
-            products_data = Products.get(_id)
-            if products_data is None:
-                msg = "user does not exist"
-                return {'data': msg, 'ok': False}, http.BAD_REQUEST
-            _id = Products(products_data, _id=_id).patch(data)
-            return {'data': str(_id), 'ok': True}, http.CREATED
-        except StatusException as e:
-            return {'data': f"Error patching: {e}", 'ok': False}, e.status
