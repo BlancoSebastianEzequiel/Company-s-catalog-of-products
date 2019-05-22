f688ae633b6a71983cd95b2b2a04ed131fb6bcc8
sebastian <sebastian.e.blanco@gmail.com>
2019-05-22 11:47:46 -0300

    create product data base model

---
+++ server/model/products.py
@@ -0,0 +1,26 @@
+from server.model.base import Model
+
+
+class Products(Model):
+    db_name = 'products'
+
+    schema = {
+        'code': str,
+        'name': str,
+        'description': str,
+        'images': list,
+        'size': str,  # (70 ml, 100 ml, 150 ml, 250 ml).
+        'active_principle': str
+    }
+
+    unique_values = {
+        'code': True,
+        'name': False,
+        'description': False,
+        'images': False,
+        'size': False,
+        'active_principle': False,
+    }
+
+    def __init__(self, data, _id=None, unique_values=False):
+        super().__init__(data, _id, check_unique_values=unique_values)
