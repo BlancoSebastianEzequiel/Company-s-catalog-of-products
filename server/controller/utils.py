f4c1bd3a3169f52e40ec9e0515753ac0300a75f3
sebastian <sebastian.e.blanco@gmail.com>
2019-05-22 03:15:36 -0300

    add dunction that send email from the company account

--- server/controller/utils.py
+++ server/controller/utils.py
@@ -1,6 +1,8 @@
-import bcrypt
-from flask import jsonify
-from server.secrets import TOKEN_SERIALIZER
+import smtplib
+from flask import jsonify, current_app
+from server.secrets import TOKEN_SERIALIZER, COMPANY_EMAIL, EMAIL_PASSWORD
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 # pylint: disable=C0103
@@ -10,12 +12,16 @@ def response(data, ok, **kwargs):
 
 
 def get_hashed_password(secret_password):
-    encoded_password = secret_password.encode('utf-8')
-    return bcrypt.hashpw(encoded_password, bcrypt.gensalt(12)).decode()
+    data = {'password': secret_password}
+    return generate_token_from_data(data)
 
 
 def check_password(secret_password, hashed):
-    return bcrypt.checkpw(secret_password.encode('utf-8'), hashed.encode())
+    return hashed == get_hashed_password(secret_password)
+
+
+def get_secret_password_from_hashed(hashed):
+    return get_data_from_token(hashed)['password']
 
 
 def get_data_from_token(token):
@@ -24,3 +30,24 @@ def get_data_from_token(token):
 
 def generate_token_from_data(data):
     return TOKEN_SERIALIZER.dumps(data).decode('utf-8')
+
+
+def send_email(email_to, subject, message):
+    if current_app.config['SKIP_SEND_EMAIL']:
+        return
+    # create message object instance
+    msg = MIMEMultipart()
+    # setup the parameters of the message
+    msg['From'] = COMPANY_EMAIL
+    msg['To'] = email_to
+    msg['Subject'] = subject
+    # add in the message body
+    msg.attach(MIMEText(message, 'plain'))
+    # create server
+    server = smtplib.SMTP('smtp.gmail.com: 587')
+    server.starttls()
+    # Login Credentials for sending the mail
+    server.login(msg['From'], EMAIL_PASSWORD)
+    # send the message via the server.
+    server.sendmail(msg['From'], msg['To'], msg.as_string())
+    server.quit()
