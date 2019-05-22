561e04b541fcc8fd8e5f0892ed232e3975723172
sebastian <sebastian.e.blanco@gmail.com>
2019-05-22 05:28:29 -0300

    add password recovery data to print on the posting formulary

---
+++ client/src/data/passwordRecoveryData.js
@@ -0,0 +1,18 @@
+const fieldsState = {
+  email: ''
+}
+const fields = [
+  {
+    placeholder: 'something@gmail.com',
+    fieldName: 'email',
+    title: 'Email',
+    type: 'email'
+  }
+]
+
+const data = {
+  'fields': fields,
+  'fieldsState': fieldsState
+}
+
+export default data
