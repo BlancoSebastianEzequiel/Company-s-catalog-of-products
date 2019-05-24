1f38b78c49f39fa3600ef59147cc3889e434d115
sebastian <sebastian.e.blanco@gmail.com>
2019-05-23 22:41:23 -0300

    use generic list container passing the corresponding arguments for each one

---
+++ client/src/containers/ClientListContainer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ListContainer from './ListContainer'
+
+export default class ClientListContainer extends React.Component {
+
+  writeInfo = (aClient) => {
+    return {
+      'data': 'USER NAME: ' + aClient['user_name'] + ' -- EMAIL: ' + aClient['email'],
+      '_id': aClient['_id']
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <ListContainer
+          url='/users/'
+          query='/users/?type=client'
+          writeInfo={(aClient) => this.writeInfo(aClient)}
+          urlToRedirect='/modify-client'
+        />
+      </div>
+    )
+  }
+}
---
+++ client/src/containers/ProductListContainer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ListContainer from './ListContainer'
+
+export default class ClientListContainer extends React.Component {
+
+  writeInfo = (aProduct) => {
+    return {
+      'data': 'Code: ' + aProduct['code'] + ' -- Name: ' + aProduct['name'],
+      '_id': aProduct['_id']
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <ListContainer
+          url='/products/'
+          query='/products/'
+          writeInfo={(aProduct) => this.writeInfo(aProduct)}
+          urlToRedirect='/modify-product'
+        />
+      </div>
+    )
+  }
+}
