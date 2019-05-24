0a11dd0facaa7c0e5b37502139abdf4b20fccd1c
sebastian <sebastian.e.blanco@gmail.com>
2019-05-23 22:41:50 -0300

    add product data to display in formularies

---
+++ client/src/data/productData.js
@@ -0,0 +1,54 @@
+const fieldsState = {
+  code: '',
+  name: '',
+  description: '',
+  images: [],
+  size: '',
+  active_principle: ''
+}
+
+const fields = [
+  {
+    placeholder: '200',
+    fieldName: 'code',
+    title: 'Code',
+    type: 'text'
+  },
+  {
+    placeholder: 'paracetamol',
+    fieldName: 'name',
+    title: 'Name',
+    type: 'text'
+  },
+  {
+    placeholder: 'analgésicos y antiinflamatorios',
+    fieldName: 'description',
+    title: 'Description',
+    type: 'text'
+  },
+  {
+    placeholder: '',
+    fieldName: 'images',
+    title: 'Images',
+    type: 'text'
+  },
+  {
+    placeholder: '70ml',
+    fieldName: 'size',
+    title: 'Size',
+    type: 'text'
+  },
+  {
+    placeholder: '300',
+    fieldName: 'active_principle',
+    title: 'Active principle code',
+    type: 'text'
+  }
+]
+
+const data = {
+  'fields': fields,
+  'fieldsState': fieldsState
+}
+
+export default data
